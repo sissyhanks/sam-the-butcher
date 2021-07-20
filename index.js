65d8230f119225280ae736aefbd65334329f0e3b
Bernadette McKnight <sissyhanks@yahoo.com>
2021-07-20 13:50:11 -0400

    console.table intigration

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
@@ -10,7 +11,7 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT * FROM role', function (error, results) {
+connection.query('SELECT * FROM role JOIN department ON role.department_id = department.id', function (error, results) {
   if (error) throw error;
   connection.connect((err) => {
     console.log(results);
