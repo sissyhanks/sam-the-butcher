29524584315cdfe78e198e4217e6e47620578b6a
Bernadette McKnight <sissyhanks@yahoo.com>
2021-07-20 14:27:59 -0400

    inquirer functionality

--- index.js
+++ index.js
@@ -11,9 +11,41 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT * FROM role JOIN department ON role.department_id = department.id', function (error, results) {
+connection.query('SELECT * FROM role JOIN department ON role.department_id = department.id JOIN employee ON role.id = employee.role_id', function (error, results) {
   if (error) throw error;
   connection.connect((err) => {
-    console.log(results);
+    console.log("welcome to my tracker!")
+    console.table(results);
+    mainMenu();
   });
 });
+
+
+function mainMenu(){
+  inquirer.prompt([
+    {
+      type:"list",
+      message:"What would you like to do ?",
+      name:"useroption",
+      choices:["View Department","View Positions","View Employee","Exit"]
+    }
+  ]).then(function({useroption}){
+    console.log(useroption)
+    switch(useroption){
+      case "View Department":
+          viewDepartment();
+          break;
+      default:
+          connection.end();
+          process.exit(0);
+    }
+  })
+}
+
+function viewDepartment(){
+  connection.query("SELECT * FROM department",function(err, data){
+    if(err) throw err;
+    console.table(data);
+    mainMenu()
+  })
+}
