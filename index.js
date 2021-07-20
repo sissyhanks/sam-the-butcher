e0619dcf7610e1694bbdff9152aaaf817a8f10ad
Bernadette McKnight <sissyhanks@yahoo.com>
2021-07-20 14:51:59 -0400

    add employee function

--- index.js
+++ index.js
@@ -27,13 +27,16 @@ function mainMenu(){
       type:"list",
       message:"What would you like to do ?",
       name:"useroption",
-      choices:["View Department","View Positions","View Employee","Exit"]
+      choices:["View Department","View Positions","View Employee","Add Employee","Exit"]
     }
   ]).then(function({useroption}){
     console.log(useroption)
     switch(useroption){
       case "View Department":
           viewDepartment();
+          break;
+      case "Add Employee":
+          addEmployee();
           break;
       default:
           connection.end();
@@ -47,5 +50,68 @@ function viewDepartment(){
     if(err) throw err;
     console.table(data);
     mainMenu()
+  })
+}
+
+function addEmployee(){
+  connection.query("select * from role",function(err, results){
+    if(err) throw err;
+    let rolesDB = results.map(role => {
+      return({
+        value: role.id,
+        name: role.title
+      })
+    })
+    inquirer.prompt([
+      {
+        type:"input",
+        name:"firstName",
+        message:"Enter First name "
+      },
+      {
+        type:"input",
+        name:"lastName",
+        message: "enter last name"
+      },
+      {
+        type:"list",
+        name:"roleid",
+        message:"Choose role",
+        choices: rolesDB
+      },
+      {
+        type:"list",
+        name:"managerid",
+        message:"chose manager",
+        choices:[
+          {
+            value:26,
+            name:"Kwame Morales"
+          },
+          {
+            value:28,
+            name:"Saffron Lacey"
+
+          },
+          {
+            value:32,
+            name:"Khia Conner"
+          },
+          {
+            value:36,
+            name:"Edie Erickson"
+          }
+        ]
+      }
+    
+      
+    ]).then(function(response){
+      console.log(response);
+      connection.query("Insert into employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);",[response.firstName,response.lastName,response.roleid,response.managerid],function(err,data){
+        if(err) throw err;
+        console.table(data);
+        mainMenu()
+      })
+    })
   })
 }
